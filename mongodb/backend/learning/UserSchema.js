a592f9c72840d660ed337f3c525a492d1a980a0b
dev-ethar <tinayousofiali@gmail.com>
2022-10-18 13:00:58 -0700

    learned more about validation and custom validation

--- mongodb/backend/learning/UserSchema.js
+++ mongodb/backend/learning/UserSchema.js
@@ -1,9 +1,29 @@
 const mongose = require("mongoose");
 
+// and now adding custom validator using the validator object
 const userSchema = new mongose.Schema({
-    name: {type: String, required: true},
-    age: { type: Number, required: true},
-    Dob: { type: Date, required: true}
+    name: { type: String, 
+        required: true, 
+        minLength: 10, 
+        maxLength: 20, 
+        lowercase: false, 
+        uppercase: true
+    },
+    age: { 
+        type: Number, 
+        required: true, 
+        min: 10,
+        max: 21,
+        validate: {
+            validator: value => value % 2 !== 0,
+            message: props => `${props.value} is not an even number`
+        }
+    },
+    Dob: { 
+        type: Date, 
+        default: () => Date.now(), 
+        immutable: true
+    }
 });
 
 module.exports = mongose.model("users", userSchema);
